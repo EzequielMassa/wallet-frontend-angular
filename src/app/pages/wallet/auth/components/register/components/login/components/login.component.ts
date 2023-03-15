d26b6f545711a96a1561c290ecd283ab1ee280ec
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2023-03-15 09:54:07 -0300

    implemented reactive forms login

--- src/app/pages/wallet/auth/components/register/components/login/components/login.component.ts
+++ src/app/pages/wallet/auth/components/register/components/login/components/login.component.ts
@@ -1,7 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'wal-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.initializeLoginForm();
+  }
+
+  initializeLoginForm(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.loginForm.value);
+  }
+}
--- src/app/pages/wallet/auth/components/register/components/login/login.module.ts
+++ src/app/pages/wallet/auth/components/register/components/login/login.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from 'src/app/pages/wallet/auth/components/register/components/login/components/login.component';
 import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule],
+  imports: [CommonModule, LoginRoutingModule, ReactiveFormsModule],
 })
 export class LoginModule {}
