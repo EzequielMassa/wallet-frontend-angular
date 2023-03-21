19d747d8dfcbfd94f03cb5b69c1a79ee0ad02ba6
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2023-03-21 19:04:49 -0300

    created auth service

--- src/app/pages/wallet/pages/auth/auth.module.ts
+++ src/app/pages/wallet/pages/auth/auth.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import {HttpClientModule} from "@angular/common/http";
+import {AuthService} from "./services/auth.service";
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    AuthRoutingModule
-  ]
+    AuthRoutingModule,
+    HttpClientModule
+  ],
+  providers: [AuthService]
 })
 export class AuthModule { }
--- src/app/pages/wallet/pages/auth/store/types/registerRequest.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface RegisterRequestInterface {
-  user: {
-    firstname: string,
-    lastname: string,
-    email: string,
-    password: string,
-  }
-}
