fc9a55777bf0245f924d7438844a1ff5606452b7
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2023-03-21 18:08:40 -0300

    created auth reducer , appstate

--- src/app/pages/wallet/pages/auth/components/register/components/register/register.module.ts
+++ src/app/pages/wallet/pages/auth/components/register/components/register/register.module.ts
@@ -4,9 +4,16 @@ import { NgModule } from '@angular/core';
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './components/register.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {reducers} from "../../../../store/reducers/auth.reducers";
+import {StoreModule} from "@ngrx/store";
 
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [CommonModule, RegisterRoutingModule,ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    RegisterRoutingModule,
+    ReactiveFormsModule,
+    StoreModule.forFeature('auth', reducers)
+  ],
 })
 export class RegisterModule {}
---
+++ src/app/shared/types/appState.interface.ts
@@ -0,0 +1,6 @@
+import {AuthStateInterface} from "../../pages/wallet/pages/auth/store/types/authState.interface";
+
+export interface AppStateInterface {
+  auth:AuthStateInterface,
+}
+
