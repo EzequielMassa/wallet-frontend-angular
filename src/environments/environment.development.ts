386d7e42d71d7769e8fa8cba07163b9d4da4a2a4
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2023-12-13 14:34:22 -0300

    fix: changed backend host to render

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://wallet-v1-env.eba-ipztfzcp.us-east-1.elasticbeanstalk.com',
+  apiUrl: 'https://wallet-render-ev4v.onrender.com',
 };
